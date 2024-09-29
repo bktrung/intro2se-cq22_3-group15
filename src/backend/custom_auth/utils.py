6705566171cfa654647016d79a1ed5dd39e171c2
Khac Trung <22120396@student.hcmus.edu.vn>
2024-09-29 11:23:11 +0700

    Delete src/backend/custom_auth directory

--- src/backend/custom_auth/utils.py
+++
@@ -1,9 +0,0 @@
-from rest_framework_simplejwt.token_blacklist.models import OutstandingToken, BlacklistedToken
-
-def logout_user_from_other_devices(user):
-    # Get all outstanding tokens for the user
-    outstanding_tokens = OutstandingToken.objects.filter(user=user)
-    
-    # Blacklist all outstanding tokens
-    for token in outstanding_tokens:
-        BlacklistedToken.objects.get_or_create(token=token)
