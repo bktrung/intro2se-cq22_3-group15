a1067dc720b74eb67e0c2c3022caf326f4cc99ab
C0smic <vovantung0212@gmail.com>
2024-12-26 17:04:56 +0700

    refactor: Update Dockerfile and docker-compose.yml to handle celery

--- src/backend/project_management/settings.py
+++ src/backend/project_management/settings.py
@@ -172,6 +172,8 @@ EMAIL_HOST_PASSWORD = 'irqa vavd buzv brlc'
 
 CELERY_BROKER_URL = 'redis://localhost:6379/1'
 CELERY_RESULT_BACKEND = 'redis://localhost:6379/1'
+# CELERY_BROKER_URL = 'redis://redis:6379/1'
+# CELERY_RESULT_BACKEND = 'redis://redis:6379/1'
 CELERY_ACCEPT_CONTENT = ['json']
 CELERY_TASK_SERIALIZER = 'json'
 CELERY_RESULT_SERIALIZER = 'json'
