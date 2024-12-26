7212f0261a207c291d2fc7e5695ab406ebdc4cb2
Khac Trung <22120396@student.hcmus.edu.vn>
2024-12-26 22:40:44 +0700

    feature: create admin dashboard

--- src/backend/notification/models.py
+++ src/backend/notification/models.py
@@ -15,3 +15,6 @@ class NotificationLog(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='notifications')
     created_at = models.DateTimeField(auto_now_add=True)
     is_read = models.BooleanField(default=False)
+    
+    def __str__(self):
+        return self.title
