74be6fbd1907e95b0203bb6f24ab19113d67c394
txm.hien1102 <txm.hien1102@gmail.com>
2024-12-31 11:48:18 +0700

    feat: add internet checking

--- src/frontend/YouManage/app/src/main/java/com/example/youmanage/MainActivity.kt
+++ src/frontend/YouManage/app/src/main/java/com/example/youmanage/MainActivity.kt
@@ -1,5 +1,10 @@
 package com.example.youmanage
 
+import android.content.IntentFilter
+import android.net.ConnectivityManager
+import android.net.Network
+import android.net.NetworkCapabilities
+import android.net.NetworkRequest
 import android.os.Build
 import android.os.Bundle
 import android.util.Log
@@ -8,7 +13,14 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
 import androidx.annotation.RequiresApi
+import androidx.compose.material3.AlertDialog
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import com.example.youmanage.navigation.RootNavGraph
+import com.example.youmanage.screens.components.ErrorDialog
 import com.example.youmanage.ui.theme.YouManageTheme
 import com.example.youmanage.viewmodel.ActivityLogsViewModel
 import com.google.firebase.FirebaseApp
@@ -17,12 +29,16 @@ import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+    private var isNetworkConnected by mutableStateOf(true)
+
 
 
     @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
+
         FirebaseMessaging.getInstance().token.addOnCompleteListener { task ->
             if (!task.isSuccessful) {
                 Log.w("MainActivity", "Fetching FCM registration token failed", task.exception)
@@ -31,6 +47,24 @@ class MainActivity : ComponentActivity() {
             val token = task.result
             Log.d("MainActivity", "FCM Registration token: $token")
         }
+
+        val connectivityManager = getSystemService(ConnectivityManager::class.java)
+        val networkRequest = NetworkRequest.Builder()
+            .addCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET)
+            .build()
+
+        connectivityManager?.registerNetworkCallback(
+            networkRequest,
+            object : ConnectivityManager.NetworkCallback() {
+                override fun onAvailable(network: Network) {
+                    isNetworkConnected = true
+                }
+
+                override fun onLost(network: Network) {
+                    isNetworkConnected = false
+                }
+            }
+        )
 
         //val viewModel: ProjectManagementViewModel by viewModels()
 
@@ -115,6 +149,16 @@ class MainActivity : ComponentActivity() {
                 //ProjectDetailScreen()
                 //FindUserScreen()
                 RootNavGraph()
+
+                if (!isNetworkConnected) {
+                    ErrorDialog(
+                        title = "No Internet Connection",
+                        content = "Please check your internet connection and try again.",
+                        showDialog = true,
+                        onDismiss = {finish() },
+                        onConfirm = {}
+                    )
+                }
                 //TestPieChart()
 
                 // TaskDetailScreen()
